9d79da09e9385d906cd0c09faf79cfa1a43be294
memset0 <34177126+memset0@users.noreply.github.com>
2021-05-11 02:46:59 +0800

    fix.
    开黑啦的 api 疑似又修好了

--- src/modules/sender.ts
+++ src/modules/sender.ts
@@ -1,11 +1,13 @@
 import { Session } from 'koishi';
 
 export async function sendMessageList(session: Session, messageList) {
-	if (session.platform === 'kaiheila') {
-		for await (const message of messageList) {
-			await session.send(message);
-		}
-	} else {
-		await session.send(messageList.join('\n'));
-	}
+	await session.send(messageList.join('\n'));
+
+	// if (session.platform === 'kaiheila') {
+	// 	for await (const message of messageList) {
+	// 		await session.send(message);
+	// 	}
+	// } else {
+	// 	await session.send(messageList.join('\n'));
+	// }
 }
