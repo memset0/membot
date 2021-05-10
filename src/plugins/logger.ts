a692ee9df9ccc56ff437019e0a38fca842d9eab1
memset0 <34177126+memset0@users.noreply.github.com>
2021-05-11 02:41:12 +0800

    [logger] upd.

--- src/plugins/logger.ts
+++ src/plugins/logger.ts
@@ -2,8 +2,7 @@ export default (ctx) => {
 	ctx.command('logger', '调试工具');
 
 	ctx.command('logger.session', '输出 session 信息')
-		.action((arg) => {
-			console.log(arg);
-			return JSON.stringify(arg.session);
+		.action(({ session }) => {
+			return JSON.stringify(session);
 		});
 };
