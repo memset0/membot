e420fe64ed039f28dfca6c90d2e3202fa2168c68
memset0 <34177126+memset0@users.noreply.github.com>
2021-05-11 23:57:48 +0800

    [echo] disabled.

--- src/plugins/echo.ts
+++ src/plugins/echo.ts
@@ -1,6 +1,6 @@
 export default (ctx) => {
-	ctx.command('echo <message:text>', '让 bot 成为复读机~')
-		.action((_, message) => {
-			return message;
-		});
+	// ctx.command('echo <message:text>', '让 bot 成为复读机~')
+	// 	.action((_, message) => {
+	// 		return message;
+	// 	});
 };
