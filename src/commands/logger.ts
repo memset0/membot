221deb7e937a3cab50bb0aa024352f838f120e3d
memset0 <34177126+memset0@users.noreply.github.com>
2022-02-01 01:51:51 +0800

    feat(logger): upgrade to koishi-v4

--- src/commands/logger.ts
+++ src/commands/logger.ts
@@ -1,19 +1,19 @@
-// import { User } from 'koishi';
+import { User, Context, Session } from 'koishi';
 
-// export default (ctx) => {
-// 	ctx.command('logger', '调试工具');
+export default (ctx: Context) => {
+	ctx.command('logger', '调试工具');
 
-// 	ctx.command('logger.session', '输出 session 信息')
-// 		.action(({ session }) => {
-// 			return JSON.stringify(session);
-// 		});
-	
-// 	ctx.command('logger.user', '输出 user 信息')
-// 		.action(({ session }) => {
-// 			let currentPlatform = session.platform;
-// 			let currentUserID = session.sender.userId;
-// 			let currentUser = User.create(currentPlatform, currentUserID);
-			
-// 			return JSON.stringify(currentUser);
-// 		});
-// };
+	ctx.command('logger.session', '输出 session 信息')
+		.action(({ session }) => {
+			return JSON.stringify(session, null, 2);
+		});
+
+	ctx.command('logger.userdata <userId>', '输出 user 数据')
+		.action(({ session }, userId) => {
+			let currentPlatform = session.platform;
+			let currentUserID = userId || session.userId;
+			let currentUser = ctx.database.getUser(currentPlatform, currentUserID);
+
+			return JSON.stringify(currentUser, null, 2);
+		});
+};
