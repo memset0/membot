84cbf133d620d2614022c6e9f408aee4647a6a4b
memset0 <34177126+memset0@users.noreply.github.com>
2022-01-31 20:28:03 +0800

    feat: 腾讯风控原因onebot平台屏蔽私聊消息

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ app.plugin('database-mysql', config.mysql);
 app.plugin('adapter-onebot', { bots: config.bots.onebot });
 app.plugin('adapter-kaiheila', { bots: config.bots.kaiheila });
 
+app.middleware((session, next) => {
+	if (session.platform == 'onebot' && session.type == 'message' &&
+		(session.subtype == 'private' || !session.guildId || !session.channelId)) {
+		// 由于风控等原因，qq 平台发送的私聊回话消息等应拒绝回复。
+	} else {
+		return next();
+	}
+}, true);
+
 const plugins = require('./plugins');
 for (const name in plugins) {
 	// console.log('[load plugin]', name, Object.keys(config.plugins).includes(name));
