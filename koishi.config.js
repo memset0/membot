36bf1a2b06eae700be5310c16f24413fd6330a93
memset0 <34177126+memset0@users.noreply.github.com>
2022-02-01 11:54:35 +0800

    feat: 使用命令行方式启动koishi

--- koishi.config.js
+++ koishi.config.js
@@ -1,6 +1,29 @@
-module.export = {
-	watch: {
+const fs = require('fs');
+const path = require('path');
+const config = require('./src/config').default;
+
+module.exports = {
+	nickname: config.nickname,
+	...config.options,
+
+	watch: config.watch ? {
 		root: 'src',
 		ignore: [],
-	},
+	} : {},
+
+	plugins: {
+		'database-mysql': config.mysql,
+		'adapter-onebot': { bots: config.bots.onebot },
+		'adapter-kaiheila': { bots: config.bots.kaiheila },
+	}
+};
+
+const pluginDirs = [];
+const scanDir = (dir) => fs.readdirSync(dir).map(subdir => path.join(dir, subdir));
+pluginDirs.push.apply(pluginDirs, scanDir(path.join(__dirname, './src/plugins')));
+pluginDirs.push.apply(pluginDirs, scanDir(path.join(__dirname, './src/commands')));
+for (const dir of pluginDirs) {
+	const name = path.basename(dir);
+	if (name == '_scoped') continue;
+	module.exports.plugins['./' + path.relative(__dirname, dir)] = config.plugins[name];
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const { App } = require('koishi');
 
 const config = require('./config').default;
 const app = new App(config.options);
+
 app.options.nickname = config.nickname;
-app.options.prefix = config.prefix;
 
 app.plugin('database-mysql', config.mysql);
 
@@ -11,7 +11,7 @@ app.plugin('adapter-onebot', { bots: config.bots.onebot });
 app.plugin('adapter-kaiheila', { bots: config.bots.kaiheila });
 
 app.using(['database'], (ctx) => {
-	const plugins = require('./plugins');
+	const plugins = LoadPlugins();
 	for (const name in plugins) {
 		// console.log('[load plugin]', name, Object.keys(config.plugins).includes(name));
 		if (Object.keys(config.plugins).includes(name)) {
@@ -22,4 +22,44 @@ app.using(['database'], (ctx) => {
 	}
 })
 
+console.log(app.options);
 app.start();
+
+
+function LoadPlugins() {
+	const fs = require('fs');
+	const path = require('path');
+
+	const logger = new (require('koishi').Logger)('plugin');
+
+	function scanDir(dir) {
+		return fs.readdirSync(dir).map(subdir => path.join(dir, subdir));
+	}
+
+	let plugins = {};
+	let dirs = [];
+	dirs.push.apply(dirs, scanDir(path.join(__dirname, './plugins')));
+	dirs.push.apply(dirs, scanDir(path.join(__dirname, './commands')));
+
+	for (let dir of dirs) {
+		let name = path.basename(dir);
+		if (name == '_scoped') {
+			continue;
+		}
+
+		let plugin;
+		if (name.endsWith('.ts')) {
+			name = name.slice(0, name.length - 3);
+			plugin = require(path.join(path.dirname(dir), name)).default
+		} else {
+			plugin = require(path.join(path.dirname(dir), name, './index.ts')).default;
+		}
+
+		if (plugin) {
+			logger.info('load plugin', name, 'from', dir);
+			plugins[name] = plugin;
+		}
+	}
+
+	return plugins;
+}
