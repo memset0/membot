ec3091f61629c142d2230bc98ae3e110ab5b1d27
memset0 <34177126+memset0@users.noreply.github.com>
2022-02-02 11:47:28 +0800

    fix(wenyan): 修复权限问题

--- src/commands/wenyan/index.ts
+++ src/commands/wenyan/index.ts
@@ -5,7 +5,7 @@ import * as character from './character';
 export const name = 'bot';
 
 export function apply(ctx: Context) {
-	ctx.command('wenyan', '文言文查询', { authority: 4 })
+	ctx.command('wenyan', '文言文查询')
 		.alias('wy');
 
 		character.apply(ctx);
