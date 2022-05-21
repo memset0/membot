614c729c048d6316460af226068d503027d149c1
memset0 <34177126+memset0@users.noreply.github.com>
2022-05-21 10:49:29 +0800

    Revert "fix(domcer)."
    This reverts commit 4a3327416136196227715de8dd1207d4fd032dca.

--- src/commands/domcer/api.ts
+++ src/commands/domcer/api.ts
@@ -13,13 +13,6 @@ export async function get(uri: string, data = null) {
 	if (data.uuid) data.uuid = String(data.uuid);
 	if (data.name) data.name = String(data.name);
 
-	if (data.name == '虎牙搜慕暝') {
-		data.name = '虎牙搜靳乐';
-	} else if (data.name == '虎牙搜靳乐') {
-		data.name = '虎牙搜慕暝';
-	}
-
-
 	const rsp = await request.get(root + uri).query(data);
 	return rsp.text;
 }
