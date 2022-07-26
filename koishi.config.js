898b97fc3956843cd832ae2476b2524bee82af49
memset0 <34177126+memset0@users.noreply.github.com>
2022-07-27 00:15:06 +0800

    feat(entry): 支持 config.yml 中配置全局变量

--- koishi.config.js
+++ koishi.config.js
@@ -2,9 +2,15 @@ const fs = require('fs')
 const path = require('path')
 const YAML = require('yaml')
 
+/**
+ * 引入配置文件
+ */
 const configFile = path.join(__dirname, './config.yml')
 const config = YAML.parse(fs.readFileSync(configFile).toString())
 
+/**
+ * 引入本地化补丁文件
+ */
 const i18nLocaleFile = path.join(__dirname, './src/i18n-patch.yml')
 const i18nLocale = YAML.parse(fs.readFileSync(i18nLocaleFile).toString())
 config.plugins ||= {}
@@ -14,8 +20,10 @@ config.plugins.locales.i18n = {
 	...(config.plugins.locales.i18n || {}),
 }
 
-
-module.exports = {
+/**
+ * 配置 koishi
+ */
+const koishi = {
 	...config.options,
 	minSimilarity: 0,
 	autoAuthorize: (ses) => (ses.groupId ? 1 : 0),
@@ -49,12 +57,53 @@ module.exports = {
 	}
 }
 
-const pluginDirs = []
-const scanDir = (dir) => fs.readdirSync(dir).map(subdir => path.join(dir, subdir))
-pluginDirs.push.apply(pluginDirs, scanDir(path.join(__dirname, './src/plugins')))
-pluginDirs.push.apply(pluginDirs, scanDir(path.join(__dirname, './src/commands')))
-for (const dir of pluginDirs) {
+/**
+ * 扫描并载入本地插件
+ */
+const scan = (dir) => fs.readdirSync(dir).map(subdir => path.join(dir, subdir))
+for (const dir of [
+	...scan(path.join(__dirname, './src/plugins')),
+	...scan(path.join(__dirname, './src/commands')),
+]) {
 	const name = path.basename(dir, path.extname(path.basename(dir)))
-	if (name == '_scoped') continue
-	module.exports.plugins['./' + path.relative(__dirname, dir)] = config.plugins[name]
+	if (name == '_scoped') {
+		continue
+	}
+	const pluginPath = './' + path.relative(__dirname, dir).replace(/\\/g, '/')
+	const pluginData = config.plugins[name] || {}
+	koishi.plugins[pluginPath] = pluginData
 }
+
+/**
+ * 应用全局变量
+ */
+const globals = {}
+function expand(node, key = '') {
+	if (key) {
+		globals[key] = node
+	}
+	if (typeof node === 'object') {
+		const prefix = key ? key + '.' : ''
+		for (const i in node) {
+			expand(node[i], prefix + i)
+		}
+	}
+}
+function walk(node) {
+	for (const i in node) {
+		if (typeof node[i] === 'string' &&
+			node[i].startsWith('<') &&
+			node[i].endsWith('>') &&
+			Object.keys(globals).includes(node[i].slice(1, -1))
+		) {
+			node[i] = globals[node[i].slice(1, -1)]
+		}
+		if (typeof node[i] === 'object') {
+			walk(node[i])
+		}
+	}
+}
+expand(config.globals)
+walk(koishi)
+
+module.exports = koishi
