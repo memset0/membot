6c77eed937062e1791f472c72942d26e1e3ab17b
memset0 <34177126+memset0@users.noreply.github.com>
2022-07-27 00:17:03 +0800

    fix(utils.type): 删除无用判断

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -22,7 +22,7 @@ export function deepCopy<T>(target: T): T {
 		(target as any[]).forEach((v) => { cp.push(v) })
 		return cp.map((n: any) => deepCopy<any>(n)) as any
 	}
-	if (typeof target === 'object' && target !== {}) {
+	if (typeof target === 'object') {
 		const cp = { ...(target as { [key: string]: any }) } as { [key: string]: any }
 		Object.keys(cp).forEach(k => {
 			cp[k] = deepCopy<any>(cp[k])
