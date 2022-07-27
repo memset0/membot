254fc023a2f2fea8732ba4e4d3a204902028df5d
memset0 <34177126+memset0@users.noreply.github.com>
2022-07-27 16:19:20 +0800

    feat(feedback): 在所有平台生效

--- src/plugins/feedback.ts
+++ src/plugins/feedback.ts
@@ -22,28 +22,26 @@ export async function apply(ctx: Context, config: Config) {
 	const footer = '\n警告：请不要滥用此功能，否则可能导致账号被机器人封禁。'
 
 	ctx.middleware((session: Session, next) => {
-		if (session.platform == 'onebot') {
-			const keyString = segment('at', { id: session.bot.selfId })
+		const keyString = segment('at', { id: session.bot.selfId })
 
-			if (session.content.indexOf(keyString) !== -1 && !session.content.startsWith('[CQ:quote,id=')) {
-				let content = session.content
-				if (content.replace(keyString, '').replace(/\s/g, '') == '') {
-					return `${segment('at', { id: session.author.userId })}欢迎使用机器人的反馈功能，想说什么话请直接在消息中@本机器人哦` + footer
-				}
-
-				const user = session.username ? `${session.username}(${session.author.userId})` : session.author.userId
-				const channel = session.channelName ? `${session.channelName}(${session.channelId})` : session.channelId
-				const from = session.guildId === 'private' ? `用户 ${user}` : `群聊 ${channel} 的用户 ${user}`
-
-				content = `[Feedback] 来自 ${from}\n${content.replace(keyString, '@' + ctx.options.nickname).trim()}\n`
-				content += `封禁：.bot.ban ${session.author.userId}\n`
-				content += session.guildId === 'private' ?
-					`回复：.bot.sendmsg ${session.channelId} -q ${session.messageId} (message)` :
-					`回复：.bot.sendmsg ${session.channelId} -a ${session.author.userId} -q ${session.messageId} (message)`
-
-				bot.sendPrivateMessage(config.master.userId, content)
-				return `${segment('at', { id: session.author.userId })}你的反馈已转发给主人，想说什么就继续留言哦。但如果你不文明的话……哼，小心我不理你！` + footer
+		if (session.content.indexOf(keyString) !== -1 && !session.content.startsWith('[CQ:quote,id=')) {
+			let content = session.content
+			if (content.replace(keyString, '').replace(/\s/g, '') == '') {
+				return segment('at', { id: session.author.userId }) + `欢迎使用机器人的反馈功能，想说什么话请直接在消息中@本机器人哦` + footer
 			}
+
+			const user = session.username ? `${session.username}(${session.author.userId})` : session.author.userId
+			const channel = session.channelName ? `${session.channelName}(${session.channelId})` : session.channelId
+			const from = session.guildId === 'private' ? `用户 ${user}` : `群聊 ${channel} 的用户 ${user}`
+
+			content = `[Feedback] 来自 ${from}\n${content.replace(keyString, '@' + ctx.options.nickname).trim()}\n`
+			content += `封禁：.bot.ban ${session.author.userId}\n`
+			content += session.guildId === 'private' ?
+				`回复：.bot.sendmsg ${session.channelId} -q ${session.messageId} (message)` :
+				`回复：.bot.sendmsg ${session.channelId} -a ${session.author.userId} -q ${session.messageId} (message)`
+
+			bot.sendPrivateMessage(config.master.userId, content)
+			return segment('at', { id: session.author.userId }) + `你的反馈已转发给主人，想说什么就继续留言哦。但如果你不文明的话……哼，小心我不理你！` + footer
 		}
 
 		return next()
