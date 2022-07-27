a944e5eb7667b12dae7f2e5f55a887b858ed26c5
memset0 <34177126+memset0@users.noreply.github.com>
2022-07-27 15:34:54 +0800

    feat(mcping): 新的消息模板，提升在tg上的显示效果

--- src/commands/minecraft/index.ts
+++ src/commands/minecraft/index.ts
@@ -1,21 +1,14 @@
 import { Context, Logger } from 'koishi'
 
-import ping from './ping'
-
+import * as commandPing from './ping'
 
 export const name = 'minecraft'
-export const using = []
-export const logger = new Logger('minecraft')
 
+export function apply(ctx: Context) {
+	const logger = new Logger(name)
 
-
-export default (ctx: Context) => {
 	ctx.command('minecraft', 'Minecraft 相关指令')
 		.alias('mc')
 
-	ctx.command('minecraft.ping <url:text>', '查看 MC 服务器信息')
-		.alias('mcping')
-		.action(async (_, address) => {
-			return await ping(address)
-		})
+	ctx.plugin(commandPing)
 }
--- src/commands/minecraft/ping.ts
+++ src/commands/minecraft/ping.ts
@@ -1,9 +1,35 @@
-import { Logger, segment } from 'koishi'
+import { Context, Logger, segment } from 'koishi'
 import Timeout from 'await-timeout'
 import mcping from '@l2studio/minecraft-ping'
 import { Server, JavaEditionServerStatus, BedrockEditionServerStatus } from '@l2studio/minecraft-ping'
 
-const logger = new Logger('minecraft.ping')
+
+const name = 'minecraft.ping'
+
+export async function apply(ctx: Context) {
+	ctx.command('minecraft.ping <url:text>', '查看 MC 服务器信息')
+		.alias('mcping')
+		.action(async (_, address) => {
+			const timer = new Timeout()
+			let res = null
+			res = await Promise.race([
+				adaptJavaServerMeta(address, await mcping(Server.JAVA, address)),
+				timer.set(2000, '未找到指定服务器，可能是服务器不存在或响应超时'),
+			])
+			// try {
+			// 	res = await Promise.race([
+			// 		adaptJavaServerMeta(address, await mcping(Server.JAVA, address)),
+			// 		adaptBedrockServerMeta(address, await mcping(Server.BEDROCK, address)),
+			// 		timer.set(2000, '未找到指定服务器，可能是服务器不存在或响应超时'),
+			// 	])
+			// } finally {
+			// 	timer.clear()
+			// }
+			// logger.info('res', res)
+			return res as string
+		})
+}
+
 
 function parseDescripiton(content: string) {
 	return content
@@ -13,18 +39,16 @@ function parseDescripiton(content: string) {
 
 export function adaptJavaServerMeta(address: string, meta: JavaEditionServerStatus) {
 	// logger.info('java', meta)
-	let flushString = `服务器: ${address} (Java Edition)`
+	let flushString = ``
 	if (meta.favicon) {
 		flushString += segment.image('base64://' + meta.favicon.slice('data:image/png;base64,'.length))
 	}
-	if (meta.description) {
-		flushString += `简介：${parseDescripiton(meta.description as string)}\n`
-	}
-	if (meta.players) {  // should always be true
-		flushString += `玩家：${meta.players.online} / ${meta.players.max}\n`
-	}
+	flushString += `服务器: ${address} (Java Edition) [${meta.players.online} / ${meta.players.max}]\n`
 	if (meta?.version?.name || meta?.version) {
 		flushString += `版本：${meta?.version?.name || meta?.version}\n`
+	}
+	if (meta.description) {
+		flushString += `简介：${parseDescripiton(meta.description as string)}\n`
 	}
 	return flushString
 }
@@ -32,25 +56,4 @@ export function adaptJavaServerMeta(address: string, meta: JavaEditionServerStat
 export function adaptBedrockServerMeta(address: string, meta: BedrockEditionServerStatus) {
 	// logger.info('bedrock', meta)
 	return 'bedrock'
-}
-
-
-export default async function ping(address: string) {
-	const timer = new Timeout()
-	let res = null
-	res = await Promise.race([
-		adaptJavaServerMeta(address, await mcping(Server.JAVA, address)),
-		timer.set(2000, '未找到指定服务器，可能是服务器不存在或响应超时'),
-	])
-	// try {
-	// 	res = await Promise.race([
-	// 		adaptJavaServerMeta(address, await mcping(Server.JAVA, address)),
-	// 		adaptBedrockServerMeta(address, await mcping(Server.BEDROCK, address)),
-	// 		timer.set(2000, '未找到指定服务器，可能是服务器不存在或响应超时'),
-	// 	])
-	// } finally {
-	// 	timer.clear()
-	// }
-	// logger.info('res', res)
-	return res as string
 }
