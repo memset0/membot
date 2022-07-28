3869867a792a36410a3d4ca99dc31d20d5bd5093
memset0 <34177126+memset0@users.noreply.github.com>
2022-07-28 15:41:26 +0800

    build(deps): fork adapter-telegram to local

---
+++ src/adapters/telegram/index.ts
@@ -0,0 +1,29 @@
+import { TelegramBot } from './bot'
+import * as Telegram from './types'
+
+export { Telegram }
+
+export * from './bot'
+export * from './polling'
+export * from './sender'
+export * from './server'
+export * from './utils'
+
+export default TelegramBot
+
+declare module '@satorijs/core' {
+  interface Session {
+    telegram?: Telegram.Update & Telegram.Internal
+  }
+
+  interface Events {
+    'telegram/inline-query'(session: Session): void
+    'telegram/chosen-inline-result'(session: Session): void
+    'telegram/callback-query'(session: Session): void
+    'telegram/shipping-query'(session: Session): void
+    'telegram/pre-checkout-query'(session: Session): void
+    'telegram/poll'(session: Session): void
+    'telegram/poll-answer'(session: Session): void
+    'telegram/chat-member'(session: Session): void
+  }
+}
