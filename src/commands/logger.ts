111bc278f09ac4b052fefe7c4b4f88cf5efb60ff
memset0 <34177126+memset0@users.noreply.github.com>
2022-08-04 16:27:07 +0800

    feat(logger): bug fix & update

--- src/commands/logger.ts
+++ src/commands/logger.ts
@@ -1,4 +1,4 @@
-import { User, Context, Session } from 'koishi'
+import { User, Context, Session, segment } from 'koishi'
 
 export default (ctx: Context) => {
 	ctx.command('logger', '调试工具', { authority: 3 })
@@ -8,12 +8,37 @@ export default (ctx: Context) => {
 			return JSON.stringify(session, null, 2)
 		})
 
-	ctx.command('logger.userdata <userId>', '输出 user 数据', { authority: 3 })
-		.action(({ session }, userId) => {
-			let currentPlatform = session.platform
-			let currentUserID = userId || session.userId
-			let currentUser = ctx.database.getUser(currentPlatform, currentUserID)
+	ctx.command('logger.user <userId>', '输出用户数据', { authority: 3 })
+		.action(async ({ session }, userId) => {
+			const data = await ctx.database.getUser(session.platform, userId || session.userId)
+			const text = JSON.stringify(data, null, 2)
+			if (session.platform === 'telegram') {
+				return segment('html') + '<pre><code>' + text + '</code></pre>'
+			} else {
+				return text
+			}
+		})
 
-			return JSON.stringify(currentUser, null, 2)
+	ctx.command('logger.channel <channelId>', '输出频道数据', { authority: 3 })
+		.action(async ({ session }, channelId) => {
+			const data = await ctx.database.getChannel(session.platform, channelId || session.channelId)
+			const text = JSON.stringify(data, null, 2)
+			if (session.platform === 'telegram') {
+				return segment('html') + '<pre><code>' + text + '</code></pre>'
+			} else {
+				return text
+			}
+		})
+
+	ctx.platform('onebot')
+		.command('logger.channel.member <channelId>', '输出频道列表', { authority: 3 })
+		.action(async ({ session }, channelId) => {
+			const data = await session.bot.internal.getGroupMemberList(channelId || session.channelId)
+			const text = JSON.stringify(data, null, 2)
+			if (session.platform === 'telegram') {
+				return segment('html') + '<pre><code>' + text + '</code></pre>'
+			} else {
+				return text
+			}
 		})
 }
