b0eabc85366903d0a04178c1400c3b4c0543c411
memset0 <34177126+memset0@users.noreply.github.com>
2022-09-04 12:53:32 +0800

    feat(web-navbar): fetch telegram channel name

--- src/utils/avatar.ts
+++ src/utils/avatar.ts
@@ -1,4 +1,11 @@
-import { Context } from 'koishi'
+import { Bot, Context } from 'koishi'
+
+function switchBot(ctx: Context, platform: string): null | Bot {
+	for (const bot of ctx.bots) {
+		if (bot.platform === platform) { return bot }
+	}
+	return null
+}
 
 export function getUserAvatar(platform: string, userId: string): null | string {
 	if (platform === 'onebot') {
@@ -17,7 +24,13 @@ export function getChannelAvatar(platform: string, channelId: string): null | st
 	return null
 }
 
-export function getUserName(platform: string, channelId: string, ctx?: Context): null | string {
+export function getUserName(platform: string, userId: string, ctx?: Context): null | string {
+	if (ctx && platform === 'telegram') {
+		return switchBot(ctx, 'telegram').internal.getChat({ chat_id: userId })
+			.then((chat) => {
+				return chat.title
+			})
+	}
 	return null
 }
 
@@ -26,15 +39,16 @@ export function getChannelName(platform: string, channelId: string, ctx?: Contex
 		return getUserName(platform, channelId.slice(8), ctx)
 	}
 	if (ctx && platform === 'onebot') {
-		for (const bot of ctx.bots) {
-			if (bot.platform === 'onebot') {
-				return bot.internal.getGroupInfo(channelId)
-					.then((groupInfo) => {
-						console.log(channelId, groupInfo)
-						return groupInfo.group_name
-					})
-			}
-		}
+		return switchBot(ctx, 'onebot').internal.getGroupInfo(channelId)
+			.then((groupInfo) => {
+				return groupInfo.group_name
+			})
+	}
+	if (ctx && platform === 'telegram') {
+		return switchBot(ctx, 'telegram').internal.getChat({ chat_id: channelId })
+			.then((chat) => {
+				return chat.title
+			})
 	}
 	return null
 }
