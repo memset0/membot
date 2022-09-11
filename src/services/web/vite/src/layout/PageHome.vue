1c59335bb798f10559c05107fd996ce0c663ea29
memset0 <34177126+memset0@users.noreply.github.com>
2022-09-11 14:35:17 +0800

    feat(vite): style of readme

--- src/services/web/vite/src/layout/PageHome.vue
+++ src/services/web/vite/src/layout/PageHome.vue
@@ -1,23 +1,27 @@
 <script setup lang="ts">
-// import { IconFile } from '@arco-design/web-vue/es/icon';
 const { readme } = usePageData().data
 </script>
 
 <template>
-	<a-card>
+	<a-card style="border-radius: 10px;">
 		<template #title>
 			<div class="readme-header">
+				<IconFile :style="{fontSize:'14px',marginRight:'10px'}" />
 				README.md
 			</div>
 		</template>
+		<!-- <template #extra>
+			<a-link href="">More</a-link>
+		</template> -->
 		<Markdown :html="readme" />
 	</a-card>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .readme-header {
-	color: rgb(var(--gray-5));
+	color: rgb(var(--gray-8));
 	font-size: 13px;
 	font-weight: bold;
+	padding: 20px 0px;
 }
 </style>
