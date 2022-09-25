7d31be003ba6557badda420bb3802d0af62fc5f5
memset0 <34177126+memset0@users.noreply.github.com>
2022-09-25 10:27:18 +0800

    feat: auto verifier

---
+++ src/plugins/verifier.ts
@@ -0,0 +1,17 @@
+import { Context, Session } from 'koishi'
+import verifier from '@koishijs/plugin-verifier'
+
+export function apply(ctx: Context) {
+	verifier.apply(ctx, {
+		onFriendRequest: true,
+		onGuildMemberRequest: undefined,
+		onGuildRequest: async (session: Session): Promise<boolean | void> => {
+			const user = await session.observeUser(['authority'])
+			if (user.authority >= 3) {
+				return true
+			} else if (user.authority <= 0) {
+				return false
+			}
+		}
+	})
+}
